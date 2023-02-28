f9459f61e9f1d533719a2ea47a442155c5cc0346
tanvishinde03 <shindetanvi0203@gmail.com>
2023-02-28 19:17:46 +0530

    [Tanvi] Add : UC1 - First Name Regex

---
+++ RegexProblems/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexProblems
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            RegexProgram regex = new RegexProgram();
+            regex.ValidateFirstName("Tanvi");
+
+
+
+        }
+    }
+}
---
+++ RegexProblems/Regex.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexProblems
+{
+    public class RegexProgram
+    {
+
+        public void ValidateFirstName(string name)
+        {
+            //Tanvi
+            string firstName = "^[A-Z][a-z]{3,}?";
+            if (Regex.IsMatch(name, firstName))
+            {
+
+                Console.WriteLine("first Name is matching with Regex ");
+            }
+            else
+            {
+                Console.WriteLine("first Name is not matching with Regex");
+            }
+            Console.ReadLine();
+        }
+
+
+    }
+}
